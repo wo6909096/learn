74d1f3df1fd1e45b1c12cc15d89e99f9dbd41c14
拯救地球 <498362650@qq.com>
2023-09-17 22:26:29 +0800

    ts

--- ts-learn/src/main.ts
+++ ts-learn/src/main.ts
@@ -24,7 +24,7 @@ const arr: Tuple = [1, '']
  * implements 是实现多个接口, 接口的方法一般为空的, 必须重写才能使用
  * 2、extends是继承父类，只要那个类不是声明为final或者那个类定义为abstract的就能继承
  * JAVA中不支持多重继承，但是可以用接口 来实现，这样就要用到implements，继承只能继承一个类，
- */
+
 interface IPerson {
   name: string;
   age: number;
@@ -41,7 +41,7 @@ class Person implements IPerson {
 /**
  * 对象
  */
-const obj1: IPerson = {
+const obj1: IPerson2 = {
   name: '',
   age: 2
 }
@@ -128,5 +128,72 @@ obj3.age = 2
 
 /**
  * 条件类型
+ * 用于条件判断时的 extends ；当 extends 用于表示条件判断时，可以总结出以下规律
+ */
+/**
+ * 若位于 extends 两侧的类型相同，则 extends 在语义上可理解为 ==
  */
 type res = 1 extends 2 ? true : false
+type result = 'abc' extends 'abc' ? true : false
+/**
+ * 若位于 extends 右侧的类型包含位于 extends 左侧的类型(即狭窄类型 extends 宽泛类型)时，结果为 true，反之为 false。可以参考如下例子
+ */
+type result1 = '2' extends string | 2 ? true : false
+/**
+ * 当 extends 作用于对象时，若在对象中指定的 key 越多，则其类型定义的范围越狭窄。可以参考如下例子:
+ */
+type result2 = {a: true, b: false, c: ''} extends { a: true, b: false } ? 'a' : 'x'
+
+
+/**
+ * 在泛型类型中使用条件类型
+ * https://segmentfault.com/a/1190000040769584 参考
+ */
+
+type Demo<T, U> = T extends U ? never : T
+/**
+ * 结合用于条件判断时的 extends，可知 'a' | 'b' | 'c' extends 'a' 是 false, 因此 Demo<'a' | 'b' | 'c', 'a'> 结果是 'a' | 'b' | 'c' 么?
+ */
+/**
+ * 当条件类型作用于泛型类型时，联合类型会被拆分使用。
+ * 即 Demo<'a' | 'b' | 'c', 'a'> 会被拆分为 'a' extends 'a'、'b' extends 'a'、'c' extends 'a'。
+ * 用伪代码表示类似于:
+ * function Demo(T, U) {
+ * return T.map(val => {
+ *  if (val !== U) return val
+ *  return 'never'
+  })
+}
+
+Demo(['a', 'b', 'c'], 'a') // ['never', 'b', 'c']
+ */
+const aad: Demo<'a' | 'b' | 'c', 'a'> = 'b';
+/**
+ * 此外根据 never 类型的定义 —— never 类型可分配给每种类型，但是没有类型可以分配给 never(除了 never 本身)。即 never | 'b' | 'c' 等价于 'b' | 'c'。
+ * 因此 Demo<'a' | 'b' | 'c', 'a'> 的结果并不是 'a' | 'b' | 'c' 而是 'b' | 'c'。
+ */
+const adaff: Demo<never | '', 'a'> = ''
+
+/**
+ * 工具类型
+ * Demo 类型的声明过程其实就是 TypeScript 官方提供的工具类型中 Exclude<Type, ExcludedUnion> 的实现原理，其用于将联合类型 ExcludedUnion 排除在 Type 类型之外。
+ */
+type T = Demo<'a' | 'b' | 'c' | 'd' | 'e', 'a'>
+
+/**
+ * 基于 Demo 类型定义，
+ * 进一步地还可以实现官方工具类型中的 Omit<Type, Keys>
+ * 其用于移除对象 Type中满足 keys 类型的属性值。
+*/
+namespace Omit {
+  type Omit<Type, Keys> = {
+    [P in Demo<keyof Type, Keys>]: Type
+  }
+  
+  interface Todo {
+    title: string;
+    description: string;
+    completed: boolean;
+  }
+  type T = Omit<Todo, 'description'>
+}
